8b9b247daf466124c408c56f210f633b26fbc2ec
Alisa Kuzmina <30773217+arisuaiko@users.noreply.github.com>
2023-11-19 17:34:17 +0100

    Creating first Page Objects

---
+++ src/main/java/pages/WelcomePageObject.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class WelcomePageObject {
+
+    private  WebDriver driver;
+    private Logger log;
+
+    private String pageUrl = "http://the-internet.herokuapp.com/";
+
+    private By formAuthenticationLinkLocator = By.linkText("Form Authentication");
+
+    public WelcomePageObject(WebDriver driver, Logger log){
+        this.driver = driver;
+        this.log = log;
+    }
+
+    /** Open WelcomePage with it's URL */
+    public void openPage(){
+        log.info("Opening page:" + pageUrl);
+        driver.get(pageUrl);
+        log.info("Page opened");
+    }
+
+    /** Open LoginPage by clicking on Form Authentication link */
+    public LoginPage clickFormAuthenticationLink() {
+        log.info("Clicking Form Authentication Link on Welcome Page");
+        driver.findElement(formAuthenticationLinkLocator).click();
+        return new LoginPage(driver, log);
+    }
+}
